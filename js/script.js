8030e1c149e2eb36387a6c5a5a0e0632c77c36cf
ikhlasdansantai <initesting5@gamil.com>
2022-09-22 13:13:54 +0700

    adding interactive popup while user click links

--- js/script.js
+++ js/script.js
@@ -47,3 +47,25 @@ for (let i = 0; i < accordionContainer.length; i++) {
 window.addEventListener("scroll", () => {
   window.scrollY > 150 ? document.getElementsByClassName("onscroll-profile")[0].classList.add("show") : document.getElementsByClassName("onscroll-profile")[0].classList.remove("show");
 });
+
+const links = document.querySelectorAll("a");
+links.forEach((link) => {
+  link.addEventListener("click", () => {
+    const Toast = Swal.mixin({
+      toast: true,
+      position: "bottom-start",
+      showConfirmButton: false,
+      timer: 3000,
+      timerProgressBar: true,
+      didOpen: (toast) => {
+        toast.addEventListener("mouseenter", Swal.stopTimer);
+        toast.addEventListener("mouseleave", Swal.resumeTimer);
+      },
+    });
+
+    Toast.fire({
+      icon: "success",
+      title: "Sedang Dialihkan",
+    });
+  });
+});
